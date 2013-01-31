56075c0d93c7f17d42a1561dfdc52c690ff85730
Joseph Hall <perlhoser@gmail.com>
2013-01-31 02:13:48 +0000

    Also accept mode 0400

--- saltcloud/clouds/aws.py
+++ saltcloud/clouds/aws.py
@@ -76,9 +76,9 @@ def __virtual__():
     keymode = str(
         oct(stat.S_IMODE(os.stat(__opts__['AWS.private_key']).st_mode))
     )
-    if keymode != '0600':
+    if keymode != '0400' and keymode != '0600':
         raise SaltException(
-            'The AWS key file {0} needs to be set to mode 0600\n'.format(
+            'The AWS key file {0} needs to be set to mode 0400 or 0600\n'.format(
                 __opts__['AWS.private_key']
             )
         )
