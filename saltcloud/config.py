b1d249e22236a637afcb8d0b3b1ed6219795e107
Pedro Algarvio <pedro@algarvio.me>
2013-05-27 15:52:51 +0100

    `is_provider_configured()` can now handle driver names, provider aliases and `<alias>:<driver>`.

--- saltcloud/config.py
+++ saltcloud/config.py
@@ -458,6 +458,55 @@ def is_provider_configured(opts, provider, required_keys=()):
     Check and return the first matching and fully configured cloud provider
     configuration.
     '''
+
+    # Are we being passed the profile name? Not the driver? <alias>:<driver>?
+    if ':' in provider:
+        # we're being passed <alias>:<provider>
+        alias, lprovider = provider.split(':')
+
+        if lprovider in opts['providers']:
+
+            for provider_details in opts['providers'][alias]:
+                if provider_details['provider'] != lprovider:
+                    continue
+
+                # If we reached this far, we have a matching provider, let's
+                # see if all required configuration keys are present and not
+                # None
+                skip_provider = False
+                for key in required_keys:
+                    if provider_details.get(key, None) is None:
+                        # This provider does not include all necessary keys,
+                        # continue to next one
+                        skip_provider = True
+                        break
+
+                if skip_provider:
+                    continue
+
+                # If we reached this far, the provider included all required
+                # keys
+                return provider_details
+
+    if provider in opts['providers']:
+        # We're being passed the provider alias and no :<provider>
+        # This will only work if an alias only defines one driver
+        providers = opts['providers'][provider]
+        if len(providers) > 1:
+            raise saltcloud.exceptions.SaltCloudConfigError(
+                'Specifying just {0!r} is not enough since it has settings '
+                'for multiple drivers: {1}'.format(
+                    provider,
+                    ', '.join(
+                        '{0[provider]!r}'.format(driver)
+                        for driver in providers
+                    )
+                )
+            )
+        # There's only a single driver configured, return it
+        return providers[0]
+
+    # We're most likely being passed a driver name
     for provider_details_list in opts['providers'].values():
         for provider_details in provider_details_list:
             if 'provider' not in provider_details:
